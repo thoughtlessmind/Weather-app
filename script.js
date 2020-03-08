a931202f201194a13d3c80f78b38fb35d3f44c09
Rajiv <1998irajeev@gmail.com>
2020-03-08 16:08:47 +0530

    removed spreading of content on large screens

--- script.js
+++ script.js
@@ -1,81 +1,50 @@
-window.addEventListener('load', ()=>{
-    let long;
-    let lat;
-    let locationName = document.querySelector('.location-timezone');
-    let tempValue = document.querySelector('.temperature-degree');
-    let tempDescription = document.querySelector('.temperature-description');
-    let tempIcon = document.querySelector('.icon');
+
+
+window.addEventListener('load', () =>{
+    let lat, long;
+    let htmlTemp = document.querySelector('.temperature-degree');
+    let htmlTempDescription = document.querySelector('.temperature-description');
+    let htmlTempIcon = document.querySelector('.icon');
+    let htmlLocationName = document.querySelector('.location-timezone');
+    let htmlTempUnit = document.querySelector('.temperature-unit');
+    let htmlPage = document.querySelector('.page-data');
+    let htmlAnimationWrapper = document.querySelector('.loader');
 
     if(navigator.geolocation){
         navigator.geolocation.getCurrentPosition(position=>{
             // console.log(position);
             long=position.coords.longitude;
             lat = position.coords.latitude;
+            // console.log(lat, long);
 
 
-            const apikey = "AdxFSexdbJmEJ2t5J3jWxup3HUdJu2p8";
-            const proxy = 'https://cors-anywhere.herokuapp.com/';
-            const geoKeyApi = `http://dataservice.accuweather.com/locations/v1/cities/geoposition/search?apikey=${apikey}&q=${lat}%2C${long}`;
-            let weatherApi ;
-            let weatherData;
-
-            fetch(geoKeyApi)
-                .then(response => {
+            const apikey1 = "AdxFSexdbJmEJ2t5J3jWxup3HUdJu2p8";
+            const apikey2 = "24d0a2571288360d0fdda56bca3790d4";
+            const weatherAPI = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${long}&APPID=${apikey2}&units=metric`
+            fetch(weatherAPI)
+                .then(response =>{
                     return response.json();
-                })
+                })                
                 .then(data =>{
-                    console.log(data);
-                    locationName.textContent = data.LocalizedName;
-                    console.log(data.LocalizedName)
-                    weatherApi = `${proxy}http://dataservice.accuweather.com/forecasts/v1/daily/5day/${data.Key}?apikey=${apikey}&details=true`;
-                    return weatherApi;
-                })
-                .then(weatherApi =>{
-                    fetch(weatherApi)
-                        .then(response =>{
-                            weatherData = response.json();
-                            console.log(weatherData);
-                            return weatherData
-                            
-                        })
-                        .then(data=>{
-                            console.log(weatherData);
-                            console.log(data.DailyForecasts[0].Temperature.Maximum);
-                            tempValue.textContent = data.DailyForecasts[0].Temperature.Maximum.Value;
-                            tempDescription.textContent = data.DailyForecasts[0].Day.ShortPhrase;
-                            let iconSrc = `https://developer.accuweather.com/sites/default/files/0${data.DailyForecasts[0].Day.Icon}-s.png`
-                            tempIcon.setAttribute('src', iconSrc)
-                            
-                        })
-                })
+                    // console.log(data);
+                    htmlTemp.textContent = Math.round(data.list[0].main.temp);
+                    htmlTempDescription.textContent = data.list[0].weather[0].description;
+                    let iconSrc = `http://openweathermap.org/img/wn/${data.list[0].weather[0].icon}@2x.png`
+                    htmlTempIcon.setAttribute('src', iconSrc);
+                    htmlLocationName.textContent = data.city.name;
+                    htmlTempUnit.textContent = 'C';
+                    htmlAnimationWrapper.classList.add('hide');
+                    htmlPage.classList.remove('hide');
 
-            // fetch(geoKeyApi)
-            //     .then(response =>{
-            //         return response.json();
-            //     })
-            //     .then(data =>{
-            //         console.log(data.Key);
-            //     })
-            //     .then(key =>{
-            //         weatherApi = `${proxy}http://dataservice.accuweather.com/forecasts/v1/daily/5day/3352012?apikey=${key}&details=true`;
-            //     })
-            //     .then()
-            // fetch(weatherApi)
-            //     .then(response =>{
-            //         return response.json();
-            //     })
-            //     .then(data =>{
-            //         console.log(data);
-            //     })
-            
+
+                })
+                .catch( ()=>{
+                    alert("Something is wrong. Please try again later!")
+                })
 
         });
+    }else{
+        window.alert("Location permission is required.")
     }
-});
 
-
-
-
-// <img typeof="foaf:Image" class="img-responsive" src="
-// https://developer.accuweather.com/sites/default/files/03-s.png
-// " width="75" height="45" alt="Partly Sunny" title="Partly Sunny">
+})
